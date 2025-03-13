e702b9661217ac7813f70526fc6186a0156c9762
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-03-13 13:47:23 -0400

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -1,105 +1,117 @@
 import sys
-import os.path
+import os
 from os import path
 
 def main():
     AskUser()
 
 def AskUser():
-    adminuser=str(input("1-Sign in\n2-Sign up"))
+    global filename, username, password
+    adminuser = input("1-Sign in\n2-Sign up\n")
     match(adminuser):
-        case"1":
+        case "1":
             AskUserInfo()
         case "2":
             CreateAccount()
         case default:
-            print("It's an invalid selection, please try again")
-            Askname()
+            print("It's an invalid selection, please try again.")
+            AskUser()
 
 def CreateAccount():
-    username=str(input("please create a  user name:"))
-    file
-    FileConnectivity()
-    if (fileexist=="true"):
-        print("This username has been used, please create another username")
+    username = input("Please create a username: ")
+    filename = username + ".doc"
+    FileConnectivity(filename)
+    if fileexist == "true":
+        print("This username has been used, please create a new one.")
         CreateAccount()
     else:
         AskPassword()
 
 def AskPassword():
-    password=str(input("please create a password:"))
-    repassword=str(input("please repeat your password:"))
-    if(password == repassword):
-        print("Account Create")
+    password = input("Please create a password: ")
+    lenpassword = len(password)
+    
+    repassword = input("Please repeat your password: ")
+    if password == repassword:
+        pyfile = open(filename, "w")
+        pyfile.write(username + "," + password)
+        pyfile.close()
     else:
+        print("Passwords do not match. Please try again.")
         AskPassword()
 
 def AskUserInfo():
-    global filename
-    username=str(input("Please Enter Username:"))
-    password=str(input("Please Enter Password:"))
-
-    if (useremail == "" or password == ""):
-        print("you have incorrect username or password, please try again.")
+    username = input("Please enter username: ")
+    password = input("Please enter password: ")
+    if (lenpassword <= 4):
+        print("At least 4 digit, please try again")
         AskUserInfo()
-    else:
-        filename=username + ".doc"
+    else:  
+        filename = username + ".doc"
+        FileConnectivity(filename)
+        lenpassword = len(password)
         
-    FileConnectivity()
+        if username == "" or lenpassword <= 4 or fileexist=="false":
+            print("You have entered incorrect username or password(At least 4 digit), please try again.")
+            AskUserInfo()
+        else:
+            #Go to Menu
+            print("Both are correct")       
 
-def FileConnectivity():
+def FileConnectivity(filename):
+    global fileexist
     fileDir = os.path.dirname(os.path.realpath("__file__"))
-    fileexist = bool(path.exists(filename))
-    if (fileexist == True):
-        fileex = "true"
+    if path.exists(filename):
+        fileexist = "true"
     else:
-        fileex = "false"
-
-def AskNewUser():
-    new=str(input("Are you a new user if yes enter 1, no enter 2"))
-    match(new):
-        case"1":
-            AskEdit()
-        case "2":
-            AskDepartments()
-        case default:
-            print("It's an invalid selection, please try again")
+        fileexist = "false"
 
 def AskDepartments():
     global whichchscreen
-    departments=("Welcome to Supermarket, choose one of the Departments you are interested in.\n1-Fruit\n2-Poultry\n3-Meat\n4-Beverage\n5-Frozen Foods\n6-Dietary Food (Vegetables, Salad)\n7-Kosher\n8-Halal")
-    whichchscreen=str(input(departments))
+    departments = ("Welcome to Supermarket, choose one of the Departments you are interested in.\n"
+                   "1-Fruit\n2-Poultry\n3-Meat\n4-Beverage\n5-Frozen Foods\n6-Dietary Food (Vegetables, Salad)\n7-Kosher\n8-Halal\n")
+    whichchscreen = input(departments)
     CheckDepartments()
 
 def CheckDepartments():
-    fileDir=os.path.dirname(os.path.realpath("__file__"))
+    global whichchscreen
+    fileDir = os.path.dirname(os.path.realpath("__file__"))
+
     match(whichchscreen):
         case "1":
-            filepath=fileDir+"\Fruit.py"
+            filepath = os.path.join(fileDir, "Fruit.py")
         case "2":
-            filepath=fileDir+"\Poultry.py"
+            filepath = os.path.join(fileDir, "Poultry.py")
         case "3":
-            filepath=fileDir+"\Meat.py"
+            filepath = os.path.join(fileDir, "Meat.py")
         case "4":
-            filepath=fileDir+"\Beverage.py"
+            filepath = os.path.join(fileDir, "Beverage.py")
         case "5":
-            filepath=fileDir+"\FrozenFoods.py"
+            filepath = os.path.join(fileDir, "FrozenFoods.py")
         case "6":
-            filepath=fileDir+"\DietaryFood.py"
+            filepath = os.path.join(fileDir, "DietaryFood.py")
         case "7":
-            filepath=fileDir+"\Kosher.py"
+            filepath = os.path.join(fileDir, "Kosher.py")
         case "8":
-            filepath=fileDir+"\Halal.py"
+            filepath = os.path.join(fileDir, "Halal.py")
         case default:
-            print("It's an invalid selection, please try again")
+            print("It's an invalid selection, please try again.")
             main()
-    filenamepath={
-                "__file__":filepath,
-                "__name__":"__main__",
-                }
-    with open(filepath,"rb") as file:
-        exec(compile(file.read(), filepath, "exec"), filenamepath)
-    main()
+                
+    ExecuteDepartmentFile(filepath)
 
-if __name__=="__main__":
+def ExecuteDepartmentFile(filepath):
+    if os.path.exists(filepath):
+        filenamepath = {
+            "__file__": filepath,
+            "__name__": "__main__",
+            }
+        with open(filepath, "rb") as file:
+            exec(compile(file.read(), filepath, "exec"), filenamepath)
+        main()
+    else:
+        print(f"The department file at {filepath} does not exist.")
+        main()
+
+if __name__ == "__main__":
     main()
